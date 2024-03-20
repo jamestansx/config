92521d95e75c88deecc19b0f80d7b2e2972b2ede
James Tan <jamestansx@gmail.com>
2024-03-21 00:35:14 +0800

    feat: Add neovim indent-blankline

---
+++ nvim/lua/plugins/ibl.lua
@@ -0,0 +1,12 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    event = "BufRead",
+    opts = {
+        scope = { enabled = false },
+        indent = {
+            char = "│",
+            tab_char = "│",
+        },
+    },
+}
