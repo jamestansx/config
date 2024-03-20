01167487403af59babd80e2883426b6b98829fe4
James Tan <jamestansx@gmail.com>
2024-03-21 00:48:37 +0800

    refactor: categorize plugins

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        event = "BufRead",
+        opts = {
+            scope = { enabled = false },
+            indent = {
+                char = "│",
+                tab_char = "│",
+            },
+        },
+    },
+    {
+        "utilyre/sentiment.nvim",
+        event = "VeryLazy",
+        opts = {},
+    },
+}
