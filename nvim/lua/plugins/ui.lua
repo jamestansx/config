f6a5f220f245f8039b29a69595062ce6fe2cf2d3
James Tan <jamestansx@gmail.com>
2024-03-25 00:10:12 +0800

    nvim: update ibl config

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -3,13 +3,27 @@ return {
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
         event = "BufRead",
+        cmd = { "IBLEnable", "IBLEnableScope" },
         opts = {
-            scope = { enabled = false },
+            -- TODO: setup scope once treesitter is setup
             indent = {
                 char = "│",
-                tab_char = "│",
+                highlight = "IblIndentDark",
             },
+            viewport_buffer = { min = 50 },
         },
+        config = function(_, opts)
+            local hooks = require("ibl.hooks")
+            hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                local P = require("kanagawa.colors").setup().palette
+                vim.api.nvim_set_hl(0, "IblIndentDark", {
+                    fg = P.dragonBlack5,
+                })
+            end)
+            hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+
+            require("ibl").setup(opts)
+        end,
     },
     {
         "utilyre/sentiment.nvim",
